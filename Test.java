e7f9b6cf0939081bc5cd2bee84a6f81a5f5c72b0
jaedonkey <jaedonkee@gmail.com>
2019-08-29 12:08:46 +0800

    updated test.java

--- Test.java
+++ Test.java
@@ -3,7 +3,8 @@ public class Test{
   
   //Test Client
   public static void main(String[] args){
-    System.out.println("Hello World! This was coded and contributed by Jaedon");
+    System.out.println("Hello World! This was coded and contributed and edited by Jaedon only in Jaedons branch");
+    System.out.println("It has not been merged!");
   }
 
 
